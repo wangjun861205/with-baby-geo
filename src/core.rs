8385451aa7a0b6aee4e1d0412fe035f83c03e540
wangjun861205 <444055828@qq.com>
2022-08-05 17:55:39 +0800

    20220805

--- src/core.rs
+++ src/core.rs
@@ -5,6 +5,8 @@ use std::fmt::Display;
 use std::future::Future;
 use std::pin::Pin;
 
+pub(crate) trait Key: Serialize + Display + Send + Sync {}
+
 pub(crate) trait Mutex<K> {
     fn multiple_acquire<'a>(&'a mut self, keys: Vec<K>) -> Pin<Box<dyn Future<Output = Result<(), Error>> + 'a>>
     where
@@ -22,44 +24,35 @@ pub(crate) trait Mutex<K> {
 
 pub(crate) trait Indexer<I>
 where
-    I: std::fmt::Display + Send + Sync + Clone,
+    I: std::fmt::Display + Send + Sync,
 {
     fn index(&self, latitude: f64, longitude: f64) -> I;
     fn neighbors(&self, index: I, distance: f64) -> Vec<I>;
 }
 
-pub(crate) trait Persister<I, V>
+pub(crate) trait Persister<I>
 where
     I: Serialize,
 {
-    fn insert<'a>(&'a mut self, loc: Location<I>) -> Pin<Box<dyn Future<Output = Result<V, Error>> + 'a>>;
-    fn all_by_indices(&mut self, indices: Vec<I>) -> Pin<Box<dyn Future<Output = Result<Vec<Location<I>>, Error>>>>;
-    fn query_by_distance(&mut self, latitude: f64, longitude: f64, distance: f64, page: i32, size: i32) -> Pin<Box<dyn Future<Output = Result<(Vec<Location<I>>, i64), Error>>>>;
+    fn insert<'a>(&'a mut self, loc: Location<I>) -> Pin<Box<dyn Future<Output = Result<String, Error>> + 'a>>;
+    fn query(&mut self, indices: Vec<I>, latitude: f64, longitude: f64, distance: f64, page: i32, size: i32) -> Pin<Box<dyn Future<Output = Result<(Vec<Location<I>>, i64), Error>>>>;
+    fn exists<'a>(&'a mut self, indices: Vec<I>, latitude: f64, longitude: f64, distance: f64) -> Pin<Box<dyn Future<Output = Result<bool, Error>> + 'a>>;
 }
 
-pub(crate) trait Distancer {
-    fn distance(&self, src: (f64, f64), dst: (f64, f64)) -> f64;
-}
-
-pub(crate) async fn add_location<M, I, P, D, K, V>(mutex: &mut M, indexer: I, persister: &mut P, distancer: D, latitude: f64, longitude: f64, distance: f64) -> Result<V, Error>
+pub(crate) async fn add_location<M, I, P, D, K, V>(mutex: &mut M, indexer: I, persister: &mut P, latitude: f64, longitude: f64, distance: f64) -> Result<String, Error>
 where
     M: Mutex<K>,
     I: Indexer<K>,
-    P: Persister<K, V>,
-    D: Distancer,
+    P: Persister<K>,
     K: Serialize + Display + Send + Sync + Clone + Ord,
 {
     let idx = indexer.index(latitude, longitude);
     let mut neighbors = indexer.neighbors(idx.clone(), distance);
     neighbors.sort();
     mutex.multiple_acquire(neighbors.clone()).await?;
-    let locs = persister.all_by_indices(neighbors.clone()).await?;
-    for loc in locs {
-        let dist = distancer.distance((latitude, longitude), (loc.latitude, loc.longitude));
-        if dist <= distance {
-            mutex.multiple_release(neighbors.clone()).await?;
-            return Err(Error::msg("already exists location nearby"));
-        }
+    if persister.exists(neighbors.clone(), latitude, longitude, distance).await? {
+        mutex.multiple_release(neighbors.clone()).await?;
+        return Err(Error::msg("already exists location nearby"));
     }
     let res = persister
         .insert(Location {
@@ -70,4 +63,16 @@ where
         .await?;
     mutex.multiple_release(neighbors.clone()).await?;
     Ok(res)
+}
+
+pub(crate) async fn nearby_locations<I, P, K>(indexer: &I, persister: &mut P, latitude: f64, longitude: f64, distance: f64, page: i32, size: i32) -> Result<(Vec<Location<K>>, i64), Error>
+where
+    I: Indexer<K>,
+    P: Persister<K>,
+    K: Key,
+{
+    let idx = indexer.index(latitude, longitude);
+    let indices = indexer.neighbors(idx, distance);
+    let (locs, total) = persister.query(indices, latitude, longitude, distance, page, size).await?;
+    Ok((locs, total))
 }
--- src/persisters.rs
+++ src/persisters.rs
@@ -1,31 +1,38 @@
 use crate::core::Persister;
 use crate::models::*;
-use mongodb::options::FindOptions;
+use serde::Serialize;
 
-pub(crate) struct MongoPersister {
-    coll: mongodb::Collection<Location<String>>,
+pub(crate) struct MongoPersister<I>
+where
+    I: Serialize,
+{
+    coll: mongodb::Collection<Location<I>>,
 }
 
-impl Persister<String, String> for MongoPersister {
-    fn insert<'a>(&'a mut self, loc: Location<String>) -> std::pin::Pin<Box<dyn std::future::Future<Output = Result<String, anyhow::Error>> + 'a>> {
+impl<I> Persister<I> for MongoPersister<I>
+where
+    I: Serialize,
+{
+    fn insert<'a>(&'a mut self, loc: Location<I>) -> std::pin::Pin<Box<dyn std::future::Future<Output = Result<String, anyhow::Error>> + 'a>> {
         Box::pin(async move {
             let res = self.coll.insert_one(loc, None).await?;
             Ok(res.inserted_id.to_string())
         })
     }
 
-    fn all_by_indices(&mut self, indices: Vec<String>) -> std::pin::Pin<Box<dyn std::future::Future<Output = Result<Vec<Location<String>>, anyhow::Error>>>> {
-        unimplemented!()
-    }
-
-    fn query_by_distance(
+    fn query(
         &mut self,
+        indices: Vec<I>,
         latitude: f64,
         longitude: f64,
         distance: f64,
         page: i32,
         size: i32,
-    ) -> std::pin::Pin<Box<dyn std::future::Future<Output = Result<(Vec<Location<String>>, i64), anyhow::Error>>>> {
+    ) -> std::pin::Pin<Box<dyn std::future::Future<Output = Result<(Vec<Location<I>>, i64), anyhow::Error>>>> {
+        unimplemented!()
+    }
+
+    fn exists<'a>(&'a mut self, indices: Vec<I>, latitude: f64, longitude: f64, distance: f64) -> std::pin::Pin<Box<dyn std::future::Future<Output = Result<bool, anyhow::Error>> + 'a>> {
         unimplemented!()
     }
 }
